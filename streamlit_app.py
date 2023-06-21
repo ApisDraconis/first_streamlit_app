3f83f30a177a455813808c998083bf42610c9e4f
ApisDraconis <122255961+ApisDraconis@users.noreply.github.com>
2023-06-21 14:05:46 +0530

    Update streamlit_app.py
    Imported Pandas and read my fruits csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("Pop's Diner")
 streamlit.header("Menu")
@@ -7,3 +8,6 @@ streamlit.text("Chicken Sandwich 🐔")
 streamlit.text("Toamto Soup 🥣")
 streamlit.text("Greek Salad 🥗")
 streamlit.text("Ramen 🍜")
+
+my_fruits_csv = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruits_csv)
