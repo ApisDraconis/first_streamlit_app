2b82f990f7e1b334083d0ba6adc2a12dd783ec08
ApisDraconis <122255961+ApisDraconis@users.noreply.github.com>
2023-06-21 13:16:49 +0530

    Update streamlit_app.py
    Added and edited food items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,8 @@ import streamlit
 
 streamlit.title("Pop's Diner")
 streamlit.header("Menu")
-streamlit.text("Bacon")
-streamlit.text("Milkshake")
+streamlit.text("Avacado Toast 🥑🍞")
+streamlit.text("Chicken Sandwich 🐔")
+streamlit.text("Toamto Soup 🥣")
+streamlit.text("Greek Salad 🥗")
+streamlit.text("Ramen 🍜")
