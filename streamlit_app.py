24130f54555c4861cb249db73387b7394f07e035
ApisDraconis <122255961+ApisDraconis@users.noreply.github.com>
2023-06-22 17:39:41 +0530

    Update streamlit_app.py
    revamp - Get fruit load list

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,15 +45,20 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 
+# Snowflake-related
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM fruit_load_list")
+        return my_cur.fetchall()
 
+# Main section
+streamlit.header("The fruit load list contains:")
 
-streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
-my_data_row = my_cur.fetchall()
-streamlit.header("The Fruit Load Contains:")
-streamlit.dataframe(my_data_row)
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 streamlit.header("Add a fruit")
 fruit_choice = streamlit.text_input('What fruit would you like to add')
